8027f527dc44a67c4bbe329585cc4b207a3e4a25
HOUA <ninjacowzx@gmail.com>
2022-12-20 11:53:56 +0100

    PEP8 Violations fixed to make the code more readable. That's some nice eye candy, nothing changes in the logic.
    The L in the def get() was changed to a more descriptive num_bytes in case anyone likes readable code.

--- noPlotReceiverScript.py
+++ noPlotReceiverScript.py
@@ -1,56 +1,60 @@
-import socket, struct, sys
-from datetime import timedelta
+import socket
+import struct
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("localhost", 2837))
 
 TypeFormats = [
-    "",     # null
-    "d",    # double (float64)
-    "?",    # bool
+    "",  # null
+    "d",  # double (float64)
+    "?",  # bool
     "ddd"]  # Vector3d
+
 
 class Packet:
     def __init__(self, data):
         self.data = data
         self.pos = 0
 
-    def get(self, l):  # get l bytes
-        self.pos += l
-        return self.data[self.pos - l:self.pos]
+    def get(self, num_bytes):  # get num_bytes bytes
+        self.pos += num_bytes
+        return self.data[self.pos - num_bytes:self.pos]
 
     def read(self, fmt):  # read all formatted values
-        v = self.readAll(fmt)
-        if len(v) == 0: return None
-        if len(v) == 1: return v[0]
+        v = self.read_all(fmt)
+        if len(v) == 0:
+            return None
+        if len(v) == 1:
+            return v[0]
         return v
 
-    def readAll(self, fmt):  # read multiple formatted values
+    def read_all(self, fmt):  # read multiple formatted values
         return struct.unpack(fmt, self.get(struct.calcsize(fmt)))
 
     @property
     def more(self):  # is there more data?
         return self.pos < len(self.data)
 
-def readPacket(dat):
+
+def read_packet(dat):
     p = Packet(dat)
 
-    messageType = p.read("B")
+    message_type = p.read("B")
 
-    if messageType == 1:
-        timestamp = p.read("Q")
+    if message_type == 1:
         while p.more:
-            nameLen = p.read("I")
-            name = p.get(nameLen).decode('utf-8')
+            name_len = p.read("I")
+            name = p.get(name_len).decode('utf-8')
 
             tp = p.read("B")
             if tp == 4:
                 # Text
-                textLen = p.read("I")
-                val = "'{0}'".format(p.get(textLen).decode('utf-8').replace("\\", "\\\\").replace("'", "\\'"))
+                text_len = p.read("I")
+                val = "'{0}'".format(p.get(text_len).decode('utf-8').replace("\\", "\\\\").replace("'", "\\'"))
             else:
-                structFormat = TypeFormats[tp]
-                val = p.read(structFormat)
+                struct_format = TypeFormats[tp]
+                val = p.read(struct_format)
 
             if name == "Altitude":
                 unit = " M"
@@ -63,7 +67,8 @@ def readPacket(dat):
                 f.truncate(0)
                 f.write("{0}: {1}{2}\n".format(name, val, unit))
 
+
 sys.stderr.write("Starting Client...\n")
 while 1:
     d, a = s.recvfrom(2048)
-    readPacket(d)
+    read_packet(d)
